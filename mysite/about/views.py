d9908a6b9e066d07ef44ee7fa1b2f60763d78f40
svet-nikolova <1004630n@student.gla.ac.uk>
2017-01-26 10:05:11 +0000

    final changes from chapter 4  + chapter 5 started

--- mysite/about/views.py
+++ mysite/about/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br/> <a href='/rango'>Index</a>")
+
+    return render(request, 'about/about.html')
