a4c7a87bb0d4d46d3dc6f09c16836c5ca88b7aa2
svet-nikolova <1004630n@student.gla.ac.uk>
2017-02-15 14:56:04 +0000

    updating templates

--- mysite/rango/urls.py
+++ mysite/rango/urls.py
@@ -1,9 +1,10 @@
 import about as about
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'rango'
 urlpatterns = [
                   url(r'^$', views.index, name='index'),
                   url(r'^about/', about.about, name='about'),
