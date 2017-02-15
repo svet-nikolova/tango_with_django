9907d2fab4cc72b2b1577a18fc72cc0f207e9d7a
svet-nikolova <1004630n@student.gla.ac.uk>
2017-02-15 23:09:19 +0000

    finishing chapter 9

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -4,6 +4,8 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 def index(request):
@@ -201,3 +203,15 @@ def user_login(request):
 
         # No context variables to pass to the template system, hence the # blank dictionary object...
         return render(request, 'rango/login.html', {})
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return HttpResponseRedirect(reverse('index'))
