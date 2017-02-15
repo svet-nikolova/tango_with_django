229c7577c617840c3182e8deb30be1b3dfd17921
svet-nikolova <1004630n@student.gla.ac.uk>
2017-02-15 22:42:48 +0000

    adding sign up and sign in pages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
--- mysite/rango/admin.py
+++ mysite/rango/admin.py
@@ -1,15 +1,17 @@
 
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
+
 
 # Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
-# Update the registration to include this customised interface
 
+# Update the registration to include this customised interface
 class PageAdmin (admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
