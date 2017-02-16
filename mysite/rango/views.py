b8526eb5f1e747c962742ab9c31d463999db7ca9
svet-nikolova <1004630n@student.gla.ac.uk>
2017-02-16 10:15:07 +0000

    exercises for chapter 10

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -16,26 +16,6 @@ def get_server_side_cookie(request, cookie, default_val=None):
     return val
 
 
-# Updated the function definition
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-
-        # update the last visit cookie now that we have updated the count
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        visits = 1
-
-        # set the last visit cookie
-        request.session['last_visit'] = last_visit_cookie # Update/set the visits cookie
-    request.session['visits'] = visits
-
-
 def visitor_cookie_handler(request):
     # Get the number of visits to the site.
     # We use the COOKIES.get() function to obtain the visits cookie.
@@ -62,7 +42,7 @@ def visitor_cookie_handler(request):
 
 
 def index(request):
-
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
@@ -73,8 +53,14 @@ def index(request):
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
 
-    return render(request, 'rango/about.html')
+    visitor_cookie_handler(request)
+    context_dict = {'visits': request.session['visits']}
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
